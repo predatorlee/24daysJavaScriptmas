a807c9fdd651c86dc7eaf72a8df9a538b7f8ba08
predatorlee <wendykgoordeen@outlook.com>
2021-12-02 16:04:34 -0400

    changes to javascript

--- daytwo/script.js
+++ daytwo/script.js
@@ -6,6 +6,7 @@ const snowBtn = document.getElementById("snow")
 // Task:
 //- Add the functionality to switch the theme between 'Christmas' and 'snow'.
 
+  
 // Stretch goals:
 // - Add more themes!
 // - Allow the user to customise the themes.
