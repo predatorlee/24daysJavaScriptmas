e8bc5297f5ec4493edba87da250f694f56d51295
predatorlee <wendykgoordeen@outlook.com>
2021-12-02 11:54:47 -0400

    changes to script file

---
+++ daytwo/script.js
@@ -0,0 +1,12 @@
+const body = document.getElementById("body")
+const greeting = document.getElementById("greeting")
+const christmasBtn = document.getElementById("christmas")
+const snowBtn = document.getElementById("snow")
+
+// Task:
+//- Add the functionality to switch the theme between 'Christmas' and 'snow'.
+
+// Stretch goals:
+// - Add more themes!
+// - Allow the user to customise the themes.
+// - Turn the radio buttons into a toggle switch.
