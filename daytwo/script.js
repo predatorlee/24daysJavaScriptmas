d1e5c1340c84059cf6e0fe8e4c522573c65e530b
predatorlee <wendykgoordeen@outlook.com>
2021-12-02 21:19:18 -0400

    changes to the script code

--- daytwo/script.js
+++ daytwo/script.js
@@ -1,8 +1,23 @@
 const body = document.getElementById("body")
 const greeting = document.getElementById("greeting")
-const christmasBtn = document.getElementById("christmas")
-const snowBtn = document.getElementById("snow")
 
+const christmasBtn = document.getElementById("christmas")
+christmasBtn.addEventListener("click", christmassify)
+
+const snowBtn = document.getElementById("snow")
+snowBtn.addEventListener("click", snowify)
+
+function christmassify() {
+  body.classList.remove("snow");  
+  body.classList.add("christmas");
+  greeting.innerHTML = `🎅Merry Christmas!`
+}
+
+function snowify() {
+  body.classList.remove("christmas");
+  body.classList.add("snow");
+  greeting.innerHTML = `Let it Snow! ☃️`
+}
 // Task:
 //- Add the functionality to switch the theme between 'Christmas' and 'snow'.
 
