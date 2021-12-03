725321dbd67ff2be70de7a20e488f3b4a767cd9a
predatorlee <wendykgoordeen@outlook.com>
2021-12-02 20:44:52 -0400

    changes to the script file

--- daytwo/script.js
+++ daytwo/script.js
@@ -1,13 +1,20 @@
 const body = document.getElementById("body")
 const greeting = document.getElementById("greeting")
+
 const christmasBtn = document.getElementById("christmas")
+christmasBtn.addEventListener("click", christmassify)
+
 const snowBtn = document.getElementById("snow")
+snowBtn.addEventListener("click", snowify)
 
-// Task:
-//- Add the functionality to switch the theme between 'Christmas' and 'snow'.
+function christmassify() {
+  body.classList.remove("snow");  
+  body.classList.add("christmas");
+  greeting.innerHTML = `🎅Merry Christmas!`
+}
 
-  
-// Stretch goals:
-// - Add more themes!
-// - Allow the user to customise the themes.
-// - Turn the radio buttons into a toggle switch.
+function snowify() {
+  body.classList.remove("christmas");
+  body.classList.add("snow");
+  greeting.innerHTML = `Let it Snow! ☃️`
+}
