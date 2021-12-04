171008cd8c875fd7026ba79afe3b9b9a67cd968c
predatorlee <wendykgoordeen@outlook.com>
2021-12-04 12:43:48 -0400

    changes to the three js file

--- js/three.js
+++ js/three.js
@@ -1,6 +1,11 @@
 const items = ["Candles", "Decorations", "Chocolate"]
 const checklist = document.getElementById("checklist")
 
+function myFunction() {
+    var x = document.createElement("INPUT");
+    x.setAttribute("type", "checkbox");
+    document.body.appendChild(x);
+}
 // Task:
 // - For each item in the items array, create a div with a class of "checklist-item", which contains a checkbox input and corresponding label.
 // - Make sure that the shopping list can render a checkbox for all the items, even if new items are added to the items array.
